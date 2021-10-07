cbe095b7be8096949af6e0e961d2df593927f500
JoeHalasz <jhalasz@yahoo.com>
2021-10-07 19:19:44 -0400

    ui stuff

--- backend.py
+++ backend.py
@@ -1,18 +1,22 @@
 
+class school:
+	schoolName = ""
+	buildings = []
+
 
 
 class Photo:
+	filePath = ""
+
 	# not sure what needs to be here yet. This will store the information on a photo and the photo itself.
 	# maybe store this in code with just a link to a file?
 	pass
 
 
-
-
 class Building:
 	buildingName = ""
 	roomList = [] 
-	buildingLocationData = [] 
+	buildingStreetAddress = ""
 
 	def numRooms():
 		return len(roomList)
@@ -25,6 +29,10 @@ class Room:
 	mainRoomPhoto = Photo() # this is the photo that can go with the room when it is searched 
 	roomPhotos = []
 	roomReviews = []
+	specialFeatures = [] # this is a list of pairs where first is the 
+						 # special feature and second is the number of 
+						 # times that has been inputed as a special feature
+
 
 	def numReviews():
 		return len(roomReviews)
@@ -35,10 +43,15 @@ class Room:
 
 
 class Review:
-	reviewerUserName = "Anonymous" # this is the default for when a user is not logged in
+	reviewerUsername = "Anonymous" # this is the default for when a user is not logged in
 	room = Room() # the room that the review is for 
 	reviewText = "" # the actual review
 	reviewPhotos = [] # any photos that go with the review
 	specialFeatures = [] # a list of special features that the room has
 	numStars = 0; # the number of stars a room has out of 5.
+
+
+	createNewRoom():
+
+
 
