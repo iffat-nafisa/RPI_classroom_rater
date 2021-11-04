ec8825b229fee668e115a27c450160b287d75c4f
iffat-nafisa <iffat.nafisa1@gmail.com>
2021-11-04 19:04:05 -0400

    added rooms.py

--- classroomRater/app.py
+++ classroomRater/app.py
@@ -5,6 +5,7 @@ from peewee import SqliteDatabase
 from models import *
 from flask_sqlalchemy import SQLAlchemy
 from views import views
+from rooms import rooms
 
 db = SQLAlchemy()
 DB_NAME = 'classroomrater.db'
@@ -19,7 +20,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 db.init_app(app)
 
 app.register_blueprint(views, url_prefix='/')
-
+app.register_blueprint(rooms, url_prefix='/')
 
 
 def create_database(app):
