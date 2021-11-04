09bc036f0b0a46bcfd4444c3ab83b188eac3c99b
jai510 <90217574+jai510@users.noreply.github.com>
2021-11-04 04:05:55 -0400

    html files updated

--- classroomRater/room.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-rooms = Blueprint('rooms', __name__)
-
-
-@views.route('/', methods=['GET', 'POST'])
-def homepage():
-    return "<h1> Test </h1>"
-    #return render_template("index.html")
--- classroomRater/rooms.py
+++ classroomRater/rooms.py
@@ -1,12 +1,32 @@
-from flask import Blueprint
+from typing import Text
+from flask import Blueprint, render_template, request
+import sys
 
 #Create Blueprint
 rooms = Blueprint('rooms', __name__)
 
 @rooms.route('/addRoom', methods=['GET', 'POST'])
 def addRoom():
-    return "<h1> Testing addReview </h1>"
+    if request.method == "POST":
+        featureList_2 = request.form["featureList"]
+        print("COPY THAT")
+        print(featureList_2)
+    else:
+        print("==================>>>>>>>>>>========================>", file=sys.stderr)
+    print("HERE_SSS")
+    return render_template("addReview.html")
 
 @rooms.route('/viewRoom', methods=['GET', 'POST'])
+
+
 def viewRoom():
-    return "<h1> Testing viewRoom </h1>"
+    #The backend (.db file) will pass the following things to room.html, so it can be displayed in the frontend
+    #Featured Picture, Feature #1, Feature #2, Feature #3, Overall Rating, List of all the Reviews, Pictures (Only 3 will be displayed)
+
+    #pictures[0] = Main "Featured" Picture, pictures[1] = Bottom Picture_1, pictures[2] = Bottom Picture_2, pictures[3] = Bottom Picture_3
+    #pictures = ["will", "it", "work"]
+    #features = ["I hope it", "does"]
+    #overallRating = 5
+    #lReviews = []
+ 
+    return render_template("room.html")
