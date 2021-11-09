9b5635b96080392b3a004037e34470628d8ca8d2
iffat-nafisa <iffat.nafisa1@gmail.com>
2021-11-09 17:17:48 -0500

    random

--- classroomRater/rooms.py
+++ classroomRater/rooms.py
@@ -14,21 +14,21 @@ rooms = Blueprint('rooms', __name__)
 
 
 
-@rooms.route('/viewRoom', methods=['GET', 'POST'])
-def viewRoom():
-    #The backend (.db file) will pass the following things to room.html, so it can be displayed in the frontend
-    #Featured Picture, Feature #1, Feature #2, Feature #3, Overall Rating, List of all the Reviews, Pictures (Only 3 will be displayed)
+# @rooms.route('/viewRoom', methods=['GET', 'POST'])
+# def viewRoom():
+#     #The backend (.db file) will pass the following things to room.html, so it can be displayed in the frontend
+#     #Featured Picture, Feature #1, Feature #2, Feature #3, Overall Rating, List of all the Reviews, Pictures (Only 3 will be displayed)
 
-    #note: The first three elements of the list are considered to be the "featured" features shown in the room.html
-    allFeatures =["No AC", "Less Space", "Has projector"]
-    avgRating = 5
+#     #note: The first three elements of the list are considered to be the "featured" features shown in the room.html
+#     allFeatures =["No AC", "Less Space", "Has projector"]
+#     avgRating = 5
     
-    #allReviews=[]
-    #allPictures=[]
+#     #allReviews=[]
+#     #allPictures=[]
 
-    #**locals() passes all the local variables defines in this function to room.html
-    return render_template("room.html", **locals())
+#     #**locals() passes all the local variables defines in this function to room.html
+#     return render_template("room.html", **locals())
 
-@rooms.route('/createReview', methods=['GET', 'POST'])
-def createReview():
-    return render_template("addReview.html")
+# @rooms.route('/createReview', methods=['GET', 'POST'])
+# def createReview():
+#     return render_template("addReview.html")
--- classroomRater/views.py
+++ classroomRater/views.py
@@ -38,7 +38,6 @@ def checkRoomInput(room):
 
 
 
-#This function will run whenever go to the "/" root
 @views.route('/', methods=['GET', 'POST'])
 def homepage():
     addSchoolAndBuildings()
@@ -58,13 +57,35 @@ def homepage():
 
         room_exists = db.session.query(Room.number).filter_by(number=room_no).count()
         if room_exists > 0:
-            return redirect(url_for('rooms.viewRoom'))
+            return redirect(url_for('views.viewRoom',buildingName=building, roomName=room_no))
 
         room = Room(number=room_no, building_name=building)
         db.session.add(room) # add to the database 
         db.session.commit()
-        return redirect(url_for('rooms.createReview')) # redirect the user to that room page
+        return redirect(url_for('views.createReview', buildingName=building,roomName=room_no)) # redirect the user to that room page
 
 
         
     return render_template("index.html")
+
+
+
+@views.route('/viewRoom/<buildingName>/<roomName>', methods=['GET', 'POST'])
+def viewRoom(buildingName, roomName):
+    # The backend (.db file) will pass the following things to room.html, so it can be displayed in the frontend
+    # Featured Picture, Feature #1, Feature #2, Feature #3, Overall Rating, List of all the Reviews, Pictures (Only 3 will be displayed)
+
+    # note: The first three elements of the list are considered to be the "featured" features shown in the room.html
+    allFeatures =["No AC", "Less Space", "Has projector"]
+    avgRating = 5
+    
+    # allReviews=[]
+    # allPictures=[]
+
+    #**locals() passes all the local variables defines in this function to room.html
+    return render_template("room.html", **locals())
+
+
+@views.route('/createReview/<buildingName>/<roomName>', methods=['GET', 'POST'])
+def createReview(buildingName, roomName):
+    return render_template("addReview.html")
