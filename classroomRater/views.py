1b60462b9ff004e45163ad3125eec836b9d1c442
JoeHalasz <jhalasz@yahoo.com>
2021-11-09 16:59:48 -0500

    passing things to other pages

--- classroomRater/views.py
+++ classroomRater/views.py
@@ -58,7 +58,7 @@ def homepage():
 
         room_exists = db.session.query(Room.number).filter_by(number=room_no).count()
         if room_exists > 0:
-            return redirect(url_for('rooms.viewRoom'))
+            return redirect(url_for('views.viewRoom',buildingName=building, roomName=room_no))
 
         room = Room(number=room_no, building_name=building)
         db.session.add(room) # add to the database 
@@ -68,3 +68,26 @@ def homepage():
 
         
     return render_template("index.html")
+
+
+@views.route('/createReview/<buildingName>/<roomName>', methods=['GET', 'POST'])
+def reviewpage(buildingName, roomName):
+    pass
+    
+
+
+
+@views.route('/viewRoom/<buildingName>/<roomName>', methods=['GET', 'POST'])
+def viewRoom(buildingName, roomName):
+    # The backend (.db file) will pass the following things to room.html, so it can be displayed in the frontend
+    # Featured Picture, Feature #1, Feature #2, Feature #3, Overall Rating, List of all the Reviews, Pictures (Only 3 will be displayed)
+
+    # note: The first three elements of the list are considered to be the "featured" features shown in the room.html
+    allFeatures =["No AC", "Less Space", "Has projector"]
+    avgRating = 5
+    
+    # allReviews=[]
+    # allPictures=[]
+
+    #**locals() passes all the local variables defines in this function to room.html
+    return render_template("room.html", **locals())
